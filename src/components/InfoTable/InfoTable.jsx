68b8a3cdc40b29ded402ea2af8ac2c5735868db2
Sergey <ishellyp@gmail.com>
2020-01-28 19:05:04 +0300

    feat: added fetch small arr and styled table

--- src/components/InfoTable/InfoTable.jsx
+++ src/components/InfoTable/InfoTable.jsx
@@ -1,7 +1,27 @@
 import React from "react"
-
-export default () => {
+import "./infoTable.css"
+export default ({ info }) => {
     return (
-        <div></div>
+        <section className="wrapper-table">
+            <table className="info-table">
+                <thead><tr>
+                    <th>id</th>
+                    <th>firstName</th>
+                    <th>lastName</th>
+                    <th>email</th>
+                    <th>phone</th>
+                </tr>
+                </thead>
+                <tbody>
+                    {info.map(el => (<tr key={el.id + Math.random()}>
+                        <td>{el.id}</td>
+                        <td>{el.firstName}</td>
+                        <td>{el.lastName}</td>
+                        <td>{el.email}</td>
+                        <td>{el.phone}</td>
+                    </tr>))}
+                </tbody>
+            </table>
+        </section>
     )
 }
--- src/components/InfoTable/InfoTableContainer.jsx
+++ src/components/InfoTable/InfoTableContainer.jsx
@@ -1,21 +1,21 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import InfoTable from "./InfoTable"
 import Loader from "../Loader/Loader"
 
 const InfoTableContainer = () => {
+    const [info, setInfo] = useState([]);
     useEffect(() => {
         const fetchTableApi = async () => {
-            const res = await fetch(`http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}`)
-            const data = await (res.json())
-            console.log(data)
+            const res = await fetch(`http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D`)
+            const data = await (res.json());
+            setInfo(data);
         }
-        fetchTableApi();
-
-
-        ;
+        fetchTableApi()
     }, [])
+    console.log(info)
+
     return (
-        <InfoTable className="wrapper-table"></InfoTable>
+        <InfoTable className="wrapper-table" info={info}></InfoTable>
     )
 }
 
